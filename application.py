2e4adc9257610d9348173691210c5f5d9d48c1a0
widoc84 <sysadmin84@gmail.com>
2019-02-16 12:25:19 +0300

    Переход на pytest с unittest для теста add_user

--- application.py
+++ application.py
@@ -49,5 +49,32 @@ class Applicaton:
 
         wd.find_element_by_id("LoginForm").submit()
 
+    def add_user(self, user):
+        wd = self.wd
+        wd.find_element_by_link_text("add new").click()#openuser
+        #edit form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(user.username)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(user.last_name)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(user.nickname)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(user.title)
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(user.tel)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(user.mail)
+        wd.find_element_by_xpath("//input[21]").click()#confirm
+        wd.find_element_by_link_text("home").click()
+        time.sleep(5)#check
+
     def destroy(self):
         self.wd.quit()
--- test_user.py
+++ test_user.py
@@ -1,83 +1,17 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import pytest
 from user import User
+from application import Applicaton
+
+@pytest.fixture
+def app(request):
+    fixture = Applicaton()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-class UntitledTestCase(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Chrome()
-        self.wd.implicitly_wait(30)
-
-    def test_add_user(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.add_user(wd, User(username="name", last_name="last_name", nickname="nicknames", title="title", tel="7777777",
+def test_add_user(app):
+    app.login(username="admin", password="secret")
+    app.add_user(User(username="name", last_name="last_name", nickname="nicknames", title="title", tel="7777777",
                  mail="test@ya.ru"))
-        self.logout(wd)
-
-    def add_user(self, user):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()#openuser
-        #edit form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(user.username)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(user.last_name)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(user.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(user.title)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(user.tel)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(user.mail)
-        wd.find_element_by_xpath("//input[21]").click()#confirm
-        wd.find_element_by_link_text("home").click()
-        time.sleep(5)#check
-
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-
-        wd.find_element_by_id("LoginForm").submit()
-
-
-    def is_element_present(self, how, what):
-        try:
-            self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.wd.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+    app.logout()
