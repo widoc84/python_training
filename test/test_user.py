26992a5d1c85937c1a425e5a1b09ee5b7c5bc0cc
widoc84 <sysadmin84@gmail.com>
2019-04-10 23:33:43 +0400

    Задание 21 проверка информаации о всех контактах

--- test/test_user.py
+++ test/test_user.py
@@ -57,6 +57,19 @@ def test_check_random_contact(app,db):
     assert merge_phones_like_on_home_page(contact_from_home_page) == merge_phones_like_on_home_page(contact_from_edit_page)
     assert merge_email_like_on_home_page(contact_from_home_page) == merge_email_like_on_home_page(contact_from_edit_page)
 
+def test_check_all_contact(app,db):
+    old_user = sorted(db.get_user_list(),key=User.id_or_nmx)
+    for i in range(len(old_user)):
+        index = i
+        contact_from_home_page = old_user[index]
+        index2 = contact_from_home_page.id
+        contact_from_edit_page = app.user.get_user_info_from_edit(index2)
+        assert contact_from_home_page.last_name == contact_from_edit_page.last_name
+        assert contact_from_home_page.username == contact_from_edit_page.username
+        assert contact_from_home_page.address == contact_from_edit_page.address
+        assert merge_phones_like_on_home_page(contact_from_home_page) == merge_phones_like_on_home_page(contact_from_edit_page)
+        assert merge_email_like_on_home_page(contact_from_home_page) == merge_email_like_on_home_page(contact_from_edit_page)
+
 
 def test_phones_on_home_page(app):
     contact_from_home_page = app.user.get_user_list()[0]
