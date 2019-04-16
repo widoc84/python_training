6432c955bf4362f221a5a2103d3be63489957d5a
widoc84 <sysadmin84@gmail.com>
2019-04-16 23:23:29 +0300

    Задание 29 allure отчёты 2

--- test/test_group.py
+++ test/test_group.py
@@ -2,32 +2,33 @@
 from model.group import Group
 import random
 import pytest
+import allure
 
 
 def test_add_group(app, db, json_groups):
-    with pytest.allure.step('Get new Group'):
+    with allure.step('Get new Group'):
         group = json_groups
-    with pytest.allure.step('Get old Groups'):
+    with allure.step('Get old Groups'):
         old_groups = db.get_group_list()
-    with pytest.allure.step('Create Group with name %s' % group):
+    with allure.step('Create Group with name %s' % group):
         app.group.create(group)
-    with pytest.allure.step('Check Group'):
+    with allure.step('Check Group'):
         new_groups = db.get_group_list()
         old_groups.append(group)
         assert sorted(old_groups, key=Group.id_or_nmx) == sorted(new_groups, key=Group.id_or_nmx)
 
 def test_edit_group(app, db, json_groups, check_ui):
-    with pytest.allure.step('Get new Group'):
+    with allure.step('Get new Group'):
         group_j = json_groups
-    with pytest.allure.step('Check count Group'):
+    with allure.step('Check count Group'):
         if len(db.get_group_list()) == 0:
             app.group.create(group_j)
-    with pytest.allure.step('Get list old Groups and choice'):
+    with allure.step('Get list old Groups and choice'):
         old_groups = db.get_group_list()
         group = random.choice(old_groups)
-    with pytest.allure.step('Edit Group with name %s' % group):
+    with allure.step('Edit Group with name %s' % group):
         app.group.edit_by_id(group.id, group)
-    with pytest.allure.step('Check Group'):
+    with allure.step('Check Group'):
         new_groups = db.get_group_list()
         assert len(old_groups) == len(new_groups)
         new_groups = app.group.get_group_list()
@@ -35,17 +36,17 @@ def test_edit_group(app, db, json_groups, check_ui):
             assert sorted(old_groups, key=Group.id_or_nmx) == sorted(new_groups, key=Group.id_or_nmx)
 
 def test_delete_group(app, db, json_groups, check_ui):
-    with pytest.allure.step('Get new Group'):
+    with allure.step('Get new Group'):
         group = json_groups
-    with pytest.allure.step('Check count Group'):
+    with allure.step('Check count Group'):
         if len(db.get_group_list()) == 0:
             app.group.create(group)
-    with pytest.allure.step('Get list old Groups and choice'):
+    with allure.step('Get list old Groups and choice'):
         old_groups = db.get_group_list()
         group = random.choice(old_groups)
-    with pytest.allure.step('Edit Group with name %s' % group):
+    with allure.step('Edit Group with name %s' % group):
         app.group.delete_by_id(group.id)
-    with pytest.allure.step('Check Group'):
+    with allure.step('Check Group'):
         new_groups = db.get_group_list()
         assert len(old_groups) - 1 == len(new_groups)
         old_groups.remove(group)
